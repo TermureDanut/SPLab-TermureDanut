921cdd41d6a0075ca0c00bacff67f44cac181b09
danut <danutvaleriu.termure@gmail.com>
2023-11-14 08:45:25 +0200

    Lab5 working

---
+++ labSP/src/main/java/com/example/classes/AlignStrategy.java
@@ -0,0 +1,5 @@
+package com.example.classes;
+
+public interface AlignStrategy {
+    void render(String text);
+}
---
+++ labSP/src/main/java/com/example/classes/SubChapter.java
@@ -0,0 +1,38 @@
+package com.example.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+public class SubChapter {
+    private String name;
+    private List<Element> elements;
+    SubChapter(String name, List<Element> elements){
+        this.name=name;
+        this.elements = elements;
+    }
+    SubChapter(String name){
+        this.name = name;
+        elements = null;
+    }
+    public void createNewParagraph(String text) {
+        if(elements == null)
+            elements = new ArrayList<Element>();
+        this.elements.add(new Paragraph(text));
+    }
+    public void createNewImage(String imageName) {
+        if(elements == null)
+            elements = new ArrayList<Element>();
+        this.elements.add(new Image(imageName));
+    }
+    public void createNewTable(String tableName) {
+        if(elements == null)
+            elements = new ArrayList<Element>();
+        this.elements.add(new Table(tableName));
+    }
+    public void print(){
+        System.out.println("Subchapter name: " + name + "\n");
+        for(Element e: elements){
+            e.print();
+            System.out.println('\n');
+        }
+    }
+}
