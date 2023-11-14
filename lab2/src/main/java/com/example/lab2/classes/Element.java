32bde2a2f3b3037e6555c009d4dc8a225ad26dad
danut <danutvaleriu.termure@gmail.com>
2023-11-14 08:29:52 +0200

    project remake

--- lab2/src/main/java/com/example/lab2/classes/Element.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab2.classes;
-
-public interface Element {
-    void print();
-    void add(Element e);
-    void remove(Element e);
-    Element get(int index);
-}
--- lab2/src/main/java/com/example/lab2/classes/Picture.java
+++
@@ -1,5 +0,0 @@
-package com.example.lab2.classes;
-
-public interface Picture {
-
-}
