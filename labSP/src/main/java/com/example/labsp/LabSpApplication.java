03a51433ba9c5fea2bdb574cd28565731d263b76
danut <danutvaleriu.termure@gmail.com>
2023-11-14 08:30:36 +0200

    new project

---
+++ labSP/src/main/java/com/example/labsp/LabSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.labsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpApplication.class, args);
+	}
+
+}
