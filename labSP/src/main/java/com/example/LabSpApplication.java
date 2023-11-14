26cce3ca39f8698a1f2972c69cbdf97c0f1cddba
danut <danutvaleriu.termure@gmail.com>
2023-11-14 08:55:27 +0200

    Lab6 working

--- labSP/src/main/java/com/example/LabSpApplication.java
+++ labSP/src/main/java/com/example/LabSpApplication.java
@@ -1,29 +1,25 @@
 package com.example;
 
-import com.example.classes.*;
+import com.example.lab6.*;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class LabSpApplication {
 	public static void main(String[] args) {
-		Section cap1 = new Section("Capitolul 1");
-		Paragraph p1 = new Paragraph("Paragraph 1");
-		cap1.add(p1);
-		Paragraph p2 = new Paragraph("Paragraph 2");
-		cap1.add(p2);
-		Paragraph p3 = new Paragraph("Paragraph 3");
-		cap1.add(p3);
-		Paragraph p4 = new Paragraph("Paragraph 4");
-		cap1.add(p4);
-		System.out.println("Printing without Alignment");
-		System.out.println();
-		cap1.print();
-		p1.setAlignStrategy(new AlignCenter());
-		p2.setAlignStrategy(new AlignRight());
-		p3.setAlignStrategy(new AlignLeft());
-		System.out.println();
-		System.out.println("Printing with Alignment");
-		System.out.println();
-		cap1.print();
+		ApplicationContext context = (ApplicationContext) SpringApplication.run(LabSpApplication.class, args);
+		TransientComponent transientBean = context.getBean(TransientComponent.class);
+		transientBean.operation();
+		transientBean = context.getBean(TransientComponent.class);
+		transientBean.operation();
+		SingletonComponent singletonBean = context.getBean(SingletonComponent.class);
+		singletonBean.operation();
+		singletonBean = context.getBean(SingletonComponent.class);
+		singletonBean.operation();
+		ClientComponent c = context.getBean(ClientComponent.class);
+		c.operation();
+		c = (ClientComponent)context.getBean("clientComponent");
+		c.operation();
 	}
 }
